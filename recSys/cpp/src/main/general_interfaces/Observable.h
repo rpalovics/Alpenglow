4a83020622aad4fb4a5ecb6934d0cabfa5b5d1a0
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2017-05-31 14:24:54 +0000

    missing files

---
+++ recSys/cpp/src/main/general_interfaces/Observable.h
@@ -0,0 +1,21 @@
+#ifndef OBSERVABLE
+#define OBSERVABLE
+
+#include "Observer.h"
+#include <vector>
+
+class Observable{
+  public:
+    void attach(Observer* observer){
+      observers_.push_back(observer);
+    }
+    void notify_observers(){
+      for(auto observer: observers_){
+        observer->notify();
+      }
+    }
+  private:
+    vector<Observer*> observers_;
+};
+
+#endif
