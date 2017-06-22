e6c0713a6ab7c0928f84473e61514f5c1549249b
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2017-06-22 10:36:05 +0000

    removed old poptf model

--- recSys/cpp/src/main/models/baseline/PopularityTimeFrameModel.cpp
+++
@@ -1,9 +0,0 @@
-#include "PopularityTimeFrameModel.h"
-
-double PopularityTimeFrameModel::prediction(RecDat * rec_dat){
-  //same as PopularityModel::prediction
-  int prediction = items_.get(rec_dat->item);
-  if (prediction == 0) return 0;
-  else return log(prediction+1)/log(maxitem_+1); //maxitem_>0 -> log(maxitem_+1)>0
-}
-
