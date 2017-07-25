a14a141864713e706253f0a255b9bb3f72bedd22
Domokos Kelen <kdomokos@gmail.com>
2017-07-25 13:50:55 +0200

    model write from python

--- recSys/cpp/src/main/models/Model.cpp
+++ recSys/cpp/src/main/models/Model.cpp
@@ -1,3 +1,15 @@
 #include "Model.h"
 
+#include <fstream>
 
+void Model::write(string file_name){
+  ofstream file(file_name);
+  write(file);
+  file.close();
+}
+
+void Model::read(string file_name){
+  ifstream file(file_name);
+  read(file);
+  file.close();
+}
--- recSys/cpp/src/main/models/Model.h
+++ recSys/cpp/src/main/models/Model.h
@@ -3,6 +3,9 @@
 
 #include "../recommender_data/RecommenderData.h"
 #include <exception>
+#include <string>
+
+using namespace std;
 
 class Model{
   public:
@@ -11,6 +14,8 @@ class Model{
     virtual double prediction(RecDat* rec_dat) = 0;
     virtual void write(ofstream& file) { throw exception(); }
     virtual void read(ifstream& file) { throw exception(); }
+    void write(string file_name);
+    void read(string file_name);
     virtual void clear(){ throw exception(); }
     virtual ~Model() {}
     bool self_test(){ return true; }
