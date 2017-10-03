6d742f1be223500c9d1edef16bdafb10434520b4
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2017-10-03 10:59:51 +0200

    offline SVD++: use chain, test

--- python/alpenglow/offline/models/SvdppModel.py
+++ python/alpenglow/offline/models/SvdppModel.py
@@ -31,7 +31,7 @@ class SvdppModel(alpenglow.offline.OfflineModel):
             begin_max=self.parameter_default("begin_max", 0.01),
             dimension=self.parameter_default("dimension", 10),
             use_sigmoid=False,
-            norm_type="disabled",
+            norm_type="constant",
             gamma=1,
             user_vector_weight=0.5,
             history_weight=0.5
@@ -45,26 +45,27 @@ class SvdppModel(alpenglow.offline.OfflineModel):
         simple_updater = rs.SvdppModelUpdater()
         simple_updater.set_model(model)
 
+        point_wise = rs.ObjectiveMSE()
+        gradient_computer = rs.GradientComputerPointWise()
+        gradient_computer.set_objective(point_wise)
+        gradient_computer.set_model(model)
+        gradient_computer.add_gradient_updater(gradient_updater)
+
         negative_sample_generator = rs.UniformNegativeSampleGenerator(**self.parameter_defaults(
             negative_rate=9
         ))
         negative_sample_generator.set_train_matrix(matrix)
         negative_sample_generator.set_items(items)
+        negative_sample_generator.add_updater(gradient_computer)
 
-        point_wise = rs.ObjectiveMSE()
-        gradient_computer = rs.GradientComputerPointWise()
-        gradient_computer.set_objective(point_wise)
-        gradient_computer.set_model(model)
 
-        learner = rs.OfflineIteratingImplicitLearner(**self.parameter_defaults(
+        learner = rs.OfflineIteratingOnlineLearnerWrapper(**self.parameter_defaults(
             seed=254938879,
             number_of_iterations=20,
+            shuffle=True,
         ))
-        learner.set_gradient_computer(gradient_computer)
-        learner.set_negative_sample_generator(negative_sample_generator)
-        learner.set_model(model)
         learner.set_recommender_data(recommender_data)
-        learner.add_gradient_updater(gradient_updater)
-        learner.add_early_simple_updater(simple_updater)
+        learner.add_early_updater(simple_updater)
+        learner.add_iterate_updater(negative_sample_generator)
 
         return (model, learner)
