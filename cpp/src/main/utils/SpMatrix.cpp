5e46421db70abb8f59157e77c301e994f36d042c
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-03-01 13:28:24 +0100

    SpMatrix: google coding style

--- cpp/src/main/utils/SpMatrix.cpp
+++ cpp/src/main/utils/SpMatrix.cpp
@@ -1,14 +1,14 @@
 #include "SpMatrix.h"
 
 void SpMatrix::clear(){
-  for(uint ii=0; ii<matrix.size(); ii++){
-    if(matrix[ii]!=NULL) delete matrix[ii];
+  for(uint ii=0; ii<matrix_.size(); ii++){
+    if(matrix_[ii]!=NULL) delete matrix_[ii];
   }
-  matrix.clear();
+  matrix_.clear();
 }
 
 void SpMatrix::read_from_file(string data_file){
-  ifstream ifs (data_file.c_str());
+  ifstream ifs(data_file.c_str());
   int row_id,col_id;
   double value;
   while(ifs >> row_id >> col_id >> value) insert(row_id,col_id,value);
@@ -17,39 +17,39 @@ void SpMatrix::read_from_file(string data_file){
 
 void SpMatrix::insert(int row_id, int col_id, double value){
   resize(row_id);
-  MatrixRow * row=get(row_id);
+  MatrixRow* row=get(row_id);
   row->insert(make_pair(col_id,value));
 }
 
 void SpMatrix::update(int row_id,int col_id, double value){
   resize(row_id);
-  MatrixRow * row=get(row_id);
-  MatrixRow :: iterator it=row->find(col_id);
+  MatrixRow* row=get(row_id);
+  RowIterator it=row->find(col_id);
   if(it!=row->end()) it->second=value;
   else row->insert(make_pair(col_id,value));
 }
 
 void SpMatrix::increase(int row_id,int col_id, double value){
   resize(row_id);
-  MatrixRow * row = get(row_id);
-  MatrixRow :: iterator it = row->find(col_id);
+  MatrixRow* row = get(row_id);
+  RowIterator it = row->find(col_id);
   if(it!=row->end()) it->second+=value;
   else row->insert(make_pair(col_id,value));
 }
 
 void SpMatrix::resize(int row_id){
-  if(size()<=row_id) matrix.resize(row_id+1,NULL);
-  if(matrix[row_id]==NULL){
-    matrix[row_id]= new MatrixRow;
+  if(size()<=row_id) matrix_.resize(row_id+1,NULL);
+  if(matrix_[row_id]==NULL){
+    matrix_[row_id]= new MatrixRow;
   }
 }
 
 int SpMatrix::size(){
-  return (int) matrix.size();
+  return (int) matrix_.size();
 }
 
 double SpMatrix::get(int row_id, int col_id){
-  MatrixRow * row = get(row_id);
+  MatrixRow* row = get(row_id);
   if(row!=NULL){
     RowIterator ri = row->find(col_id);
     if(ri!=row->end()) return ri->second;
@@ -59,14 +59,14 @@ double SpMatrix::get(int row_id, int col_id){
 }
 
 
-MatrixRow * SpMatrix::get(int row_id){
-  if(size()>row_id) return matrix[row_id];
+MatrixRow* SpMatrix::get(int row_id){
+  if(size()>row_id) return matrix_[row_id];
   else return NULL;
 }
 
 int SpMatrix::row_size(int row_id){
   if(size()>row_id){
-    MatrixRow * row =get(row_id);
+    MatrixRow* row =get(row_id);
     if(row!=NULL) return (int) row->size();
     else return 0;
   }  
@@ -76,7 +76,7 @@ int SpMatrix::row_size(int row_id){
 void SpMatrix::write_into_file(string file_name){
   ofstream ofs(file_name.c_str());
   for(int row_id=0; row_id<size(); row_id++){
-    MatrixRow * row = get(row_id);
+    MatrixRow* row = get(row_id);
     if(row!=NULL){
       for(RowIterator ri = row->begin(); ri!=row->end(); ri++){
         int col_id=ri->first;
@@ -88,7 +88,7 @@ void SpMatrix::write_into_file(string file_name){
 }
 
 void SpMatrix::erase(int row_id, int col_id){
-  MatrixRow * row = get(row_id);
+  MatrixRow* row = get(row_id);
   if(row!=NULL){
     RowIterator ri = row->find(col_id);
     if(ri!=row->end()) row->erase(ri);    
@@ -96,7 +96,7 @@ void SpMatrix::erase(int row_id, int col_id){
 }
 
 bool SpMatrix::has_value(int row_id,int col_id){
-  MatrixRow * row =  get(row_id);
+  MatrixRow* row =  get(row_id);
   if(row==NULL) return false;
   else if (row->find(col_id)==row->end()) return false;
   else return true;
--- cpp/src/main/utils/SpMatrix.h
+++ cpp/src/main/utils/SpMatrix.h
@@ -1,30 +1,20 @@
 #ifndef SPMATRIX
 #define SPMATRIX
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <sstream>
-#include <math.h>
 #include <vector>
 #include <map>
-#include <utility>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-
-
-typedef map <int,double> MatrixRow;
-typedef map <int,double> :: iterator RowIterator;
+typedef map<int,double> MatrixRow;
+typedef map<int,double>::iterator RowIterator;
 
 class SpMatrix{
   public:
-    SpMatrix(){clear();};
-    ~SpMatrix(){clear();}; 
+    ~SpMatrix(){clear();} 
     int size();
     void resize(int row_id);
     void insert(int row_id, int col_id, double value);
@@ -32,14 +22,14 @@ class SpMatrix{
     void increase(int row_id, int col_id, double value);
     void erase(int row_id, int col_id);
     double get(int row_id, int col_id);
-    MatrixRow * get(int row_id);
+    MatrixRow* get(int row_id);
     void clear();
     void read_from_file(string file_name);
     void write_into_file(string file_name);
     int row_size(int row_id);
     bool has_value(int row_id,int col_id);
   private:    
-    vector < MatrixRow * >  matrix;
+    vector<MatrixRow*>  matrix_;
 };
 
 #endif
