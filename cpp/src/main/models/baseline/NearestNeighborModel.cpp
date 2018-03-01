cfe1e382527a11f079b6b6c1ab75e4064f60de9f
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-03-01 10:27:08 +0100

    NN: ranking score iterator instead of filter

--- cpp/src/main/models/baseline/NearestNeighborModel.cpp
+++ cpp/src/main/models/baseline/NearestNeighborModel.cpp
@@ -1,51 +1,74 @@
 #include "NearestNeighborModel.h"
 
-vector<pair<int,double>>* NearestNeighborModel::get_personalized_items(int user){
-  if (user != active_user_) recompute_filter(user);
-  return &active_user_filter_;
-}
-
 double NearestNeighborModel::prediction(RecDat* rec_dat){
   USER user = rec_dat->user;
   ITEM item = rec_dat->item;
-  if (user != active_user_) recompute_filter(user);
+  if (user != active_user_) recompute(user);
   return GET_VECTORMAP(active_user_predictions_,item,0.0);
 }
 
-void NearestNeighborModel::recompute_filter(int user){
-  active_user_filter_.clear();
+RankingScoreIterator* NearestNeighborModel::get_ranking_score_iterator(int user){
+  if(user==active_user_){
+    ranking_score_iterator_.reinit();
+  } else {
+    recompute(user);
+  }
+  return &ranking_score_iterator_;
+}
+void NearestNeighborModel::recompute(int user){
   active_user_predictions_.clear();
   active_user_predictions_.resize(similarities_.size());
+  nonzero_items_.clear();
 
-  auto user_history = user_histories_.get_user_history(user);
-  if (user_history!=NULL){
-    for(int user_history_index=(int)user_history->size()-1, weight_index=0;
-        user_history_index>=0;
-        user_history_index--,weight_index++){
-      if(weights_[weight_index]<=gamma_threshold_) break;
-      ITEM i = (*user_history)[user_history_index]->item;
-      //vector<pair<ITEM,double>>& j_items = GET_VECTORMAP(similarities_,i,vector<pair<ITEM,double>>);
-      vector<pair<ITEM,double>> empty_vector;
-      vector<pair<ITEM,double>>& j_items = GET_VECTORMAP(similarities_,i,empty_vector);
-      for(const auto& similarity_pair : j_items){
-        ITEM j = similarity_pair.first;
-        double similarity = similarity_pair.second;
-        SCORE initial_pred = GET_VECTORMAP(active_user_predictions_,j,0.0);
-        if(initial_pred==0){ active_user_filter_.push_back(make_pair(j,-1)); } //TODO ha weight*sim==0, hamis hozzaadas lehet
-        ADD_VECTORMAP(active_user_predictions_,j,weights_[weight_index]*similarity);
-        //cerr << "prediction i" << i << " j" << j << " s" << similarity << " w" << weights_[weight_index] << " p" << GET_VECTORMAP(active_user_predictions_,j,0) << endl;
-      }
-    }
-
-    for(auto& filter_item : active_user_filter_){
-      filter_item.second = active_user_predictions_[filter_item.first];
-    }
-    sort(
-        active_user_filter_.begin(),
-        active_user_filter_.end(),
-        [](pair<int,double> a, pair<int,double> b) -> bool
-           { return (a.second) > (b.second); }
-        );
-  }
+  recompute_prediction(user);
+  ranking_score_iterator_.set_up(active_user_predictions_, nonzero_items_);
   active_user_ = user;
 }
+
+void NearestNeighborModel::recompute_prediction(int user){
+  auto user_history = user_histories_.get_user_history(user);
+  if (user_history==NULL) return;
+  for(int user_history_index=(int)user_history->size()-1, weight_index=0;
+      user_history_index>=0;
+      user_history_index--,weight_index++){
+    if(weights_[weight_index]<=gamma_threshold_) break;
+    ITEM visited_item = (*user_history)[user_history_index]->item;
+    vector<pair<ITEM,double>> empty_vector;
+    vector<pair<ITEM,double>>& similar_items = GET_VECTORMAP(similarities_,visited_item,empty_vector);
+    for(const auto& similarity_pair : similar_items){
+      ITEM j = similarity_pair.first;
+      double similarity = similarity_pair.second;
+      SCORE initial_pred = GET_VECTORMAP(active_user_predictions_,j,-1);
+      if(initial_pred==-1){ nonzero_items_.push_back(j); }
+      ADD_VECTORMAP(active_user_predictions_,j,weights_[weight_index]*similarity);
+      //cerr << "prediction visited_item" << visited_item << " j" << j << " s" << similarity << " w" << weights_[weight_index] << " p" << GET_VECTORMAP(active_user_predictions_,j,0) << endl;
+    }
+  }
+  
+}
+
+void NearestNeighborModelRankingScoreIterator::set_up(vector<SCORE>& predictions, vector<ITEM>& active_items){
+  for(auto& item : active_items){
+    double prediction = GET_VECTORMAP(predictions,item,0.0);
+    current_scores_.push_back(make_pair(item,prediction));
+  }
+  sort(
+      current_scores_.begin(),
+      current_scores_.end(),
+      [](pair<int,double> a, pair<int,double> b) -> bool
+         { return (a.second) > (b.second); }
+      );
+}
+
+bool NearestNeighborModelRankingScoreIterator::has_next(double bound){
+  return ( (int)current_scores_.size() > counter_ &&
+           current_scores_[counter_].second >= bound );
+}
+pair<int, double> NearestNeighborModelRankingScoreIterator::get_next(){
+  return current_scores_.at(counter_++);
+}
+
+//deprecated
+//vector<pair<int,double>>* NearestNeighborModel::get_personalized_items(int user){
+//  return NULL;
+//}
--- cpp/src/main/models/baseline/NearestNeighborModel.h
+++ cpp/src/main/models/baseline/NearestNeighborModel.h
@@ -7,7 +7,20 @@
 #include "../../utils/UserHistory.h"
 #include "../../filters/ModelFilter.h"
 #include "../../recommender_data/macros.h"
+#include "../RankingScoreIterator.h"
 
+class NearestNeighborModelRankingScoreIterator : public RankingScoreIterator {
+public:
+  bool has_next(double bound) override; //TODO
+  pair<int, double> get_next() override; //TODO
+  void set_up(vector<SCORE>& predictions, vector<ITEM>& active_items);
+  void clear(){counter_=0;current_scores_.clear();}
+  void reinit(){counter_=0;}
+  int unique_items_num(){ throw exception(); } //should not be called as all scores are nonnegatives
+private:
+  vector<pair<int,double>> current_scores_;
+  int counter_;
+};
 struct NearestNeighborModelParameters {
   double gamma=-1;
   string norm="";
@@ -15,52 +28,58 @@ struct NearestNeighborModelParameters {
   double gamma_threshold=-1;
   int num_of_neighbors=-1;
 };
-class NearestNeighborModel : public Model , public ModelFilter {
-  public:
-    NearestNeighborModel(NearestNeighborModelParameters* params):
-      gamma_(params->gamma),
-      gamma_threshold_(params->gamma_threshold),
-      num_of_neighbors_(params->num_of_neighbors),
-      norm_type_(params->direction==""?"num":params->norm),
-      direction_(params->direction==""?"forward":params->direction)
-    {
-      weights_.push_back(1.0);
-    }
-    double prediction(RecDat* rec_dat) override;
-    vector<pair<int,double>>* get_personalized_items(int user) override;
-    bool self_test(){
-      bool ok = Model::self_test();
-      if(gamma_==-1){ ok=false; cerr << "NearestNeighborModel::gamma is not set properly." << endl; }
-      if(gamma_threshold_<0){ ok=false; cerr << "NearestNeighborModel::gamma_threshold is not set properly." << endl; }
-      if(num_of_neighbors_<0){ ok=false; cerr << "NearestNeighborModel::num_of_neighbors is not set properly." << endl; }
-      if(!(norm_type_=="off" or norm_type_=="sum" or norm_type_=="num")){ ok = false; cerr << "NearestNeighborModel::norm is not set properly." << endl; }
-      if(norm_type_=="sum"){ cerr << "WARNING: NearestNeighborModel::norm_type=sum is not implemented yet." << endl; }
-      if(!(direction_=="forward" or direction_=="both")){ ok = false; cerr << "NearestNeighborModel::direction is not set properly." << endl; }
-      return ok;
-    }
-  private:
-    //params
-    const double gamma_;
-    const double gamma_threshold_;
-    const int num_of_neighbors_;
-    const string norm_type_;
-    const string direction_;
+class NearestNeighborModel
+  : public Model,
+  //  public ModelFilter, //deprecated
+    virtual public RankingScoreIteratorProvider
+{
+public:
+  NearestNeighborModel(NearestNeighborModelParameters* params):
+    gamma_(params->gamma),
+    gamma_threshold_(params->gamma_threshold),
+    num_of_neighbors_(params->num_of_neighbors),
+    norm_type_(params->direction==""?"num":params->norm),
+    direction_(params->direction==""?"forward":params->direction)
+  {}
+  double prediction(RecDat* rec_dat) override;
+  RankingScoreIterator* get_ranking_score_iterator(int user) override;
+  bool self_test(){
+    bool ok = Model::self_test();
+    if(gamma_==-1){ ok=false; cerr << "NearestNeighborModel::gamma is not set properly." << endl; }
+    if(gamma_threshold_<0){ ok=false; cerr << "NearestNeighborModel::gamma_threshold is not set properly." << endl; }
+    if(num_of_neighbors_<0){ ok=false; cerr << "NearestNeighborModel::num_of_neighbors is not set properly." << endl; }
+    if(!(norm_type_=="off" or norm_type_=="sum" or norm_type_=="num")){ ok = false; cerr << "NearestNeighborModel::norm is not set properly." << endl; }
+    if(norm_type_=="sum"){ cerr << "WARNING: NearestNeighborModel::norm_type=sum is not implemented yet." << endl; }
+    if(!(direction_=="forward" or direction_=="both")){ ok = false; cerr << "NearestNeighborModel::direction is not set properly." << endl; }
+    return ok;
+  }
+  //vector<pair<int,double>>* get_personalized_items(int user) override; //deprecated
+private:
+  //params
+  const double gamma_;
+  const double gamma_threshold_;
+  const int num_of_neighbors_;
+  const string norm_type_;
+  const string direction_;
 
-    //state
-    UserHistory user_histories_;
+  //state
+  UserHistory user_histories_;
 
-    //cache
-    vector<vector<pair<ITEM,double>>> similarities_;
-    vector<pair<ITEM,SCORE>> active_user_filter_;
-    vector<SCORE> active_user_predictions_;
-    int active_user_ = -1;
-    vector<double> weights_;
+  //cache
+  vector<vector<pair<ITEM,double>>> similarities_;
+  vector<SCORE> active_user_predictions_;
+  vector<ITEM> nonzero_items_;
+  int active_user_ = -1;
+  vector<double> weights_ = {1.0};
+  NearestNeighborModelRankingScoreIterator ranking_score_iterator_;
 
-    //other
-    void recompute_filter(int user);
-    friend class NearestNeighborModelUpdater;
-    FRIEND_TEST(TestNearestNeighborModel, weights);
-    FRIEND_TEST(TestNearestNeighborModel, direction);
+  //other
+  void recompute(int user);
+  void recompute_prediction(int user);
+  friend class NearestNeighborModelUpdater;
+  FRIEND_TEST(TestNearestNeighborModel, weights);
+  FRIEND_TEST(TestNearestNeighborModel, direction);
+  
 };
 
 #endif
