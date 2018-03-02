1324b0cc2b8ac5e93971cfb3a198676eb08a9a5d
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-03-02 11:29:29 +0100

    Exp algo: prepared for test

--- cpp/src/main/models/combination/RandomChoosingCombinedModelExpertUpdater.h
+++ cpp/src/main/models/combination/RandomChoosingCombinedModelExpertUpdater.h
@@ -2,11 +2,13 @@
 #define RANDOM_CHOOSING_COMBINED_MODEL_EXPERT_UPDATER
 
 #include "RandomChoosingCombinedModel.h"
+#include "Evaluator.h"
 #include "../ModelUpdater.h"
 #include <gtest/gtest_prod.h>
 
 struct RandomChoosingCombinedModelExpertUpdaterParameters{
   double eta = 0.1;
+  string loss_type = ""; //abs, dcg, mrr, other
 };
 
 class RandomChoosingCombinedModelExpertUpdater
@@ -15,8 +17,21 @@ class RandomChoosingCombinedModelExpertUpdater
 {
 public:
   RandomChoosingCombinedModelExpertUpdater(RandomChoosingCombinedModelExpertUpdaterParameters* params)
-  : eta_(params->eta)
+  : eta_(params->eta),
+    loss_type_(params->loss_type)
   {}
+  ~RandomChoosingCombinedModelExpertUpdater(){
+    if(loss_type_!="other"){
+      for(auto evaluator : evaluators_){ delete evaluator; }
+    }
+  }
+  void set_evaluators(vector<Evaluator*> evaluators){
+    if(loss_type_!="other"){
+      cerr << "RandomChoosingCombinedModelExpertUpdater:Set loss_type=other to set evaluators." << endl;
+      return;
+    }
+    evaluators_ = evaluators;
+  }
   void set_model(RandomChoosingCombinedModel* model){model_ = model; }
   void update(RecDat* rec_dat);
   bool self_test(){
@@ -25,11 +40,19 @@ public:
       ok=false;
       cerr<<"RandomChoosingCombinedModelExpertUpdater::model is not set."<<endl;
     }
+    if(loss_type_!="other" && loss_type_!="abs" && loss_type_!="dcg" && loss_type_!="mrr"){
+      ok=false;
+    }
+    if(loss_type_!="other"){
+      ok=false;
+      cerr<<"RandomchoosingCombinedModelExpertUpdater::loss_type=" << loss_type_ << " is not implemented." << endl;
+    }
     return ok;
   }
 
 protected:
   bool autocalled_initialize(){
+    //TODO create evaluators
     //for(auto model:model_->models){
     //  RankingScoreIteratorProvider* rsi = dynamic_cast<RankingScoreIteratorProvider*>(model);
     //  iterator_models_.push_back(rsi);
@@ -39,7 +62,9 @@ protected:
 private:
   //vector<RankingScoreIteratorProvider*> iterator_models_;
   RandomChoosingCombinedModel* model_ = NULL;
+  vector<Evaluator*> evaluators_;
   double eta_ = 0;
+  string loss_type_ = "";
 };
 
 
