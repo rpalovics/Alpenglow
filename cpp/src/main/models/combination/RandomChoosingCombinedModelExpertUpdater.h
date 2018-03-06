1451156212ebeecd5ef893f3515967b0563fb393
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-03-06 16:38:24 +0100

    Exp: dcg err test

--- cpp/src/main/models/combination/RandomChoosingCombinedModelExpertUpdater.h
+++ cpp/src/main/models/combination/RandomChoosingCombinedModelExpertUpdater.h
@@ -54,7 +54,7 @@ public:
     if(loss_type_!="other" && loss_type_!="abs" && loss_type_!="dcg" && loss_type_!="mrr"){
       ok=false;
     }
-    if(loss_type_!="other" and loss_type_!="abs"){
+    if(loss_type_!="other" and loss_type_!="abs" and loss_type_!="dcg"){
       ok=false;
       cerr<<"RandomchoosingCombinedModelExpertUpdater::loss_type=" << loss_type_ << " is not implemented." << endl;
     }
