5335b6246387451dfca4d349092983aadbbf98df
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-03-20 15:34:19 +0100

    toplist: small update

--- cpp/src/main/utils/Toplist.h
+++ cpp/src/main/utils/Toplist.h
@@ -8,25 +8,28 @@
 
 using namespace std;
 
-template<typename T,bool (*F)(T,T)>
+template<typename T,bool (*f)(T,T)>
 class Toplist{
   public:
     Toplist(int max_length){
       max_length_=max_length;
-      heap_ = new priority_queue<T,vector<T>,comparator_type >(F);
+      heap_ = new priority_queue<T,vector<T>,bool(*)(T,T) >(f);
     }
     Toplist(){
-      heap_ = new priority_queue<T,vector<T>,comparator_type >(F);
+      heap_ = new priority_queue<T,vector<T>,bool(*)(T,T) >(f);
+    }
+    ~Toplist(){
+      delete heap_;
     }
     void set_max_length(int max_length){
       max_length_=max_length;
     }
     const T get_min() const {
-      if(size()==0) cerr << "Baj van" << endl;
+      if(size()==0) throw exception();
       return heap_->top();
     }
     void insert(T t){
-      if(heap_->size()<max_length_ or !F(heap_->top(),t)){
+      if(heap_->size()<max_length_ or !f(heap_->top(),t)){
         heap_->push(t);
       } 
       if(heap_->size()>max_length_) delete_min();
@@ -42,10 +45,8 @@ class Toplist{
         heap_->pop();
       }
     }
-    typedef bool (*comparator_type)(T,T);
   private:
-    priority_queue<T,vector<T>,comparator_type >* heap_;
-    //std::function<bool(T,T)> f = [](T a, T b){return a.first > b.first;};
-    int max_length_;
+    priority_queue<T,vector<T>,bool(*)(T,T) >* heap_;
+    int max_length_ = -1;
 };
 #endif
