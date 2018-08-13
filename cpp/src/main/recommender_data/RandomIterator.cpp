e083ce1ef44286e52524192d856a2d73a756bb18
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-08-13 16:58:16 +0200

    checkpoint - code compiles

--- cpp/src/main/recommender_data/RandomIterator.cpp
+++ cpp/src/main/recommender_data/RandomIterator.cpp
@@ -1,1 +1,24 @@
 #include "RandomIterator.h"
+
+RecDat* RandomIterator::get_actual(){
+  return shuffled_data_[counter_-1];
+}
+
+RecDat* RandomIterator::get(int index) const {
+  if(index>=counter_) throw exception();
+  return shuffled_data_[index];
+}
+
+RecDat* RandomIterator::get_future(int index) const {
+ return shuffled_data_[index];
+}
+RecDat* RandomIterator::next(){
+  return shuffled_data_[counter_++];
+};
+void RandomIterator::restart(){
+  if(shuffle_mode_ == "auto_shuffle") shuffle();
+  counter_ = 0;
+};
+void RandomIterator::shuffle() {
+  random_shuffle(shuffled_data_.begin(),shuffled_data_.end(),random_);
+};
--- cpp/src/main/recommender_data/RandomIterator.h
+++ cpp/src/main/recommender_data/RandomIterator.h
@@ -1,34 +1,47 @@
 #ifndef RANDOM_ITERATOR
 #define RANDOM_ITERATOR
+#include <string>
 #include "RecommenderData.h"
 #include "../utils/Random.h"
 
-class RandomIterator{
+class RandomIterator : public Initializable {
   public:
-    RandomIterator(RecommenderData* recommender_data, int seed){
-      shuffled_data_.resize(recommender_data->size());
-      for(int i=0;i<recommender_data->size();i++){
-        shuffled_data_[i]=recommender_data->get(i);
-      }
+    RandomIterator(RecommenderData* recommender_data, int seed, string shuffle_mode){
+      recommender_data_ = recommender_data;
       random_.set(seed);
-      counter_=0;
+      shuffle_mode_ = shuffle_mode;
+      initialize();
     };
-    void shuffle(){
-      counter_=0;
-      random_shuffle(shuffled_data_.begin(),shuffled_data_.end(),random_);
-    };
-    RecDat* next(){
-      return shuffled_data_[counter_++];
-    };
+    RecDat* next();
+    void restart();
+    void shuffle();
+    RecDat* get_actual();
+    RecDat* get(int index) const;
+    RecDat* get_future(int index) const;
+    double get_following_timestamp() const;
+    //common parts TODO
     bool has_next(){
       return counter_ < shuffled_data_.size();
-    };
-    void restart(){
-      counter_ = 0;
     };
   private:
     Random random_;
     vector<RecDat*> shuffled_data_;
+    string shuffle_mode_ = "auto_shuffle"; //auto_shuffle or manual_shuffle
+
+    //common parts TODO
+    bool autocalled_initialize() override {
+      //if(!parent_is_initialized_){
+      //  parent_is_initialized_=RecommenderDataIterator::autocalled_initialize();
+      //  if(!parent_is_initialized_) return false;
+      //}
+      shuffled_data_.resize(recommender_data_->size());
+      for(int i=0;i<recommender_data_->size();i++){
+        shuffled_data_[i]=recommender_data_->get(i);
+      }
+      restart();
+      return recommender_data_->is_initialized();
+    }
+    RecommenderData* recommender_data_ = NULL;
     uint counter_;
 };
 
