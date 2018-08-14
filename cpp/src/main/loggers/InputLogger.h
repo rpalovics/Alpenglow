565b7ecc602524b9518090f85df8050b585e1a98
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-08-14 17:23:36 +0200

    TestInputLogger: ok

--- cpp/src/main/loggers/InputLogger.h
+++ cpp/src/main/loggers/InputLogger.h
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <string>
+#include <gtest/gtest_prod.h>
 #include "../general_interfaces/Initializable.h"
 #include "Logger.h"
 using namespace std;
@@ -18,16 +19,22 @@ class InputLogger : public Logger, public Initializable { //SIP_NODEFAULTCTORS
       output_file_name_ = params->output_file;
     }
     void run(RecDat* rec_dat) override {
-      output_file_ << *rec_dat << endl;
+      *output_stream_ << rec_dat->time << " " << rec_dat->user << " " << rec_dat->item << " " << rec_dat->id << " " << rec_dat->score << " " << rec_dat->eval << endl;
+    }
+    bool self_test(){
+      return output_stream_!=NULL;
     }
   protected:
     bool autocalled_initialize() override {
       output_file_.open(output_file_name_.c_str());
+      output_stream_ = &output_file_;
       return true;
     }
   private:
     string output_file_name_;
     ofstream output_file_; 
+    ostream* output_stream_ = NULL;
+  FRIEND_TEST(TestInputLogger, test);
 };
 
 #endif
---
+++ cpp/src/test/loggers/TestInputLogger.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <gtest/gtest.h>
+#include <sstream>
+#include "../../main/loggers/InputLogger.h"
+
+namespace {
+
+class TestInputLogger : public ::testing::Test  {
+public:
+  TestInputLogger() {
+  }
+  virtual ~TestInputLogger() {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+  virtual void SetUp(){}
+  virtual void TearDown(){}
+};
+
+}
+
+TEST_F(TestInputLogger, test) {
+  std::stringstream out_stringstream;
+  InputLoggerParameters params;
+  params.output_file = "";
+  InputLogger logger(&params);
+  EXPECT_FALSE(logger.self_test());
+  logger.output_stream_ = &out_stringstream;
+  EXPECT_TRUE(logger.self_test());
+  RecDat rec_dat;
+  rec_dat.time = 0;
+  rec_dat.user = 1;
+  rec_dat.item = 2;
+  rec_dat.id = 0;
+  rec_dat.score = 1;
+  rec_dat.eval = 1;
+  logger.run(&rec_dat);
+  rec_dat.time = 1;
+  rec_dat.user = 1;
+  rec_dat.item = 2;
+  rec_dat.id = 2;
+  rec_dat.score = 1;
+  rec_dat.eval = 0;
+  logger.run(&rec_dat);
+  rec_dat.time = 1;
+  rec_dat.user = 1;
+  rec_dat.item = 2;
+  rec_dat.id = 3;
+  rec_dat.score = 1;
+  rec_dat.eval = 0;
+  logger.run(&rec_dat);
+  std::string out_string = out_stringstream.str();
+  EXPECT_EQ("0 1 2 0 1 1\n1 1 2 2 1 0\n1 1 2 3 1 0\n", out_string);
+}
+
+
+
+
+int main (int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
