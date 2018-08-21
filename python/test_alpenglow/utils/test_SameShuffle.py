d31660263ad09e4e66d9c7668a8987d1702ac94f
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-08-21 15:43:49 +0200

    sameshuffle: test partial shuffle (shuffle only elements with identical timestamp

--- python/test_alpenglow/utils/test_SameShuffle.py
+++ python/test_alpenglow/utils/test_SameShuffle.py
@@ -9,7 +9,18 @@ from alpenglow.utils import SameShuffle
 
 
 class TestSameShuffle(unittest.TestCase):
-    def test__get_configurations(self):
-        c = SameShuffle(input_file="python/test_alpenglow/test_data_4", output_file="random_output_file_1")
+    def test__full_shuffle(self):
+        c = SameShuffle(
+            input_file="python/test_alpenglow/test_data_4",
+            output_file="random_output_file_1",
+            shuffle_mode="same_timestamp"
+        )
+        c.run()
+    def test__same_timestamp(self):
+        c = SameShuffle(
+            input_file="python/test_alpenglow/test_data_4",
+            output_file="random_output_file_2",
+            shuffle_mode="same_timestamp"
+        )
         c.run()
 
