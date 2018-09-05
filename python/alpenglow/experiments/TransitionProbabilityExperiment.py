7af1ffb61c90fe7577c7a24aec563f2981dc4411
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2018-09-05 13:13:00 +0200

    TransitionProbExperiment: small doc fix

--- python/alpenglow/experiments/TransitionProbabilityExperiment.py
+++ python/alpenglow/experiments/TransitionProbabilityExperiment.py
@@ -3,14 +3,14 @@ import alpenglow as prs
 
 
 class TransitionProbabilityExperiment(prs.OnlineExperiment):
-    """TransitionProbabilityExperiment(mode_="normal")
+    """TransitionProbabilityExperiment(mode="normal")
 
     A simple algorithm that focuses on the sequence of items a user has visited is one that records how often users visited item i after visiting another item j. This can be viewed as particular form of the item-to-item nearest neighbor with a time decay function that is non-zero only for the immediately preceding item. While the algorithm is more simplistic, it is fast to update the transition fre- quencies after each interaction, thus all recent information is taken into account.
 
     Parameters
     ----------
     mode : string
-        The direction of transitions to be considered.
+        The direction of transitions to be considered, possible values: normal, inverted, symmetric.
     """
     def _config(self, top_k, seed):
         model = rs.TransitionProbabilityModel()
