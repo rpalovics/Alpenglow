b4eeef4d3ae39f6ebed8a77a1f428a3d8f985207
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-11 13:52:46 +0100

    Logger.h: add documentation

--- cpp/src/main/loggers/Logger.h
+++ cpp/src/main/loggers/Logger.h
@@ -7,14 +7,30 @@
 using namespace std;
 
 class Logger{
+  /**
+    Interface for loggers.
+
+    Logger instances can be set into
+    :py:class:`alpenglow.cpp.OnlineExperiment` using the
+    `add_logger()` and the `add_end_logger()` function.
+    Loggers set through `add_logger()` are called for each sample, before
+    updating the environment and models. Loggers set through `add_end_logger()`
+    are called at the end of the experiment.
+  */
   public:
-    Logger(){}
     virtual ~Logger(){}
     virtual void run(RecDat* rec_dat)=0;
+    /**
+    run(RecDat* rec_dat)
+    Method called by :py:class:`alpenglow.cpp.OnlineExperiment`.
+    */
     bool self_test(){
       bool OK = true;
       return OK;
     }
+    /**
+    Returns true.
+    */
 };
 
 
