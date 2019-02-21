edeaf2986b7999996564b7c3126f56066eb1bf11
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-21 15:07:16 +0100

    Logger: rewrite documentation

--- cpp/src/main/loggers/Logger.h
+++ cpp/src/main/loggers/Logger.h
@@ -8,21 +8,27 @@ using namespace std;
 
 class Logger{
   /**
-    Interface for loggers.
+    Interface for logging results, simulation or debug info etc.
 
-    Logger instances can be set into
-    :py:class:`alpenglow.cpp.OnlineExperiment` using the
-    `add_logger()` and the `add_end_logger()` function.
-    Loggers set through `add_logger()` are called for each sample, before
-    updating the environment and models. Loggers set through `add_end_logger()`
-    are called at the end of the experiment.
+    In the online experiment, :py:class:`alpenglow.cpp.OnineExperiment` calls
+    loggers for each sample and at the end of the experiment. See
+    :py:class:`alpenglow.cpp.OnineExperiment` for details.
+    //here we should link add_logger and add_end_logger functions
   */
   public:
     virtual ~Logger(){}
     virtual void run(RecDat* rec_dat)=0;
     /**
     run(RecDat* rec_dat)
-    Method called by :py:class:`alpenglow.cpp.OnlineExperiment`.
+
+    Logs results, siumlation info etc. It is not allowed to update the model or other simulation objects at that point.
+
+    In the online experiment, :py:class:`alpenglow.cpp.OnlineExperiment` calls this method.
+
+    Parameters
+    ----------
+    rec_dat : RecDat*
+       The newest available sample of the experiment.
     */
     bool self_test(){
       bool OK = true;
