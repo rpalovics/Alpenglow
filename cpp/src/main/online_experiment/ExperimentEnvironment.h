f9378a6d888b11066d0b2ff194834fa26bab425c
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-21 17:34:22 +0100

    ExperimentEnvironment: rewrite documentation

--- cpp/src/main/online_experiment/ExperimentEnvironment.h
+++ cpp/src/main/online_experiment/ExperimentEnvironment.h
@@ -15,79 +15,117 @@ struct OnlineExperimentParameters;
 
 class ExperimentEnvironment{
   public:
+    /**
+      Class that stores, updates and serves common simulation data and
+      parameters, e.g. length of the top list, dataset and popularity of the
+      items.
+
+      In the online experiment, the central class
+      :py:class:`alpenglow.cpp.OnlineExperiment` updates this class and the
+      common statistic containers. This class is updated after calling loggers
+      and before calling updaters (for each sample). See details there. Other
+      objects are not allowed to modify this class or the statistic containers,
+      even if they have non-const access (exception: the common Random).
+    */
     void set_parameters(OnlineExperimentParameters* params);
+    /**
+        Sets the parameters of the experiment. Called by :py:class:`alpenglow.cpp.OnlineExperiment`.
+    */
     void set_recommender_data_iterator(RecommenderDataIterator* recommender_data_iterator){
       recommender_data_iterator_=recommender_data_iterator;
     }
+    /**
+        Sets the dataset (the time series) of the experiment. Called by :py:class:`alpenglow.cpp.OnlineExperiment`.
+    */
 
     //global parameters
     int get_top_k() const { return top_k_; }
     /**
-      Returns the top list length in the current experiment. Note that not all
-      classes consider this value.
+      Returns
+      -------
+      int
+          The top list length in the current experiment. Note that not all classes consider this value.
     */
     int get_min_time() const { return min_time_; } //TODO is this parameter in use?
     /**
-      Returns the beginning timestamp of evaluation. Elements in the time series
-      before that timestamp will not be evaluated. Note that not all evaluator
-      classes consider this value.
+      Returns
+      -------
+      int
+          The beginning timestamp of evaluation. Elements in the time series before that timestamp will not be evaluated. Note that not all evaluator classes consider this value.
     */
     int get_max_time() const { return max_time_; } //TODO is this parameter in use?
     /**
-      Returns the last timestamp of evaluation. Elements in the time series
-      after that timestamp will not be evaluated. Note that not all evaluator
-      classes consider this value.
-      //The experiment will be terminated. //TODO should be, but it isn't.
+      Returns
+      -------
+      int
+          The last timestamp of evaluation. Elements in the time series after that timestamp will not be evaluated. Note that not all evaluator classes consider this value.  //The experiment will be terminated. //TODO should be, but it isn't.
     */
     bool do_exclude_known() const { return exclude_known_; }
     /**
-       Returns whether each user-item pair should be evaluated only at the first
-       occurrence, i.e., known user-item pairs should not be evaluated at
-       repeated occurrences.
+       Returns
+       -------
+       bool
+           Whether each user-item pair should be evaluated only at the first occurrence, i.e., known user-item pairs should not be evaluated at repeated occurrences.
     */
 
     //common objects
     RecommenderDataIterator* get_recommender_data_iterator() const { return recommender_data_iterator_; } //TODO const
     /**
-      Returns the data iterator containing the time series of the experiment.
+      Returns
+      -------
+      RecommenderDataIterator*
+          A pointer to the data iterator containing the time series of the experiment. 
     */
     Random* get_random() { return &random_; }
     /**
-      Returns the common random generator.
+      Returns
+      -------
+      Random*
+          A pointer to the common random generator.
     */
 
     //common statistics
     bool is_item_new_for_user() const { return item_new_for_user_; }
     /**
-      Returns whether the current item is new for the current user, i.e., this is the
-      first occurrence of this user-item pair in the time series. Note that the
-      value is updated only when the loggers had been called already.
+      Returns
+      -------
+      bool
+          Whether the current item is new for the current user, i.e., this is the first occurrence of this user-item pair in the time series. Note that the value is updated only when the loggers had been called already.
     */
     vector<int>* get_items() { return &items_; } //TODO return const value, classes should not change the vector
     /**
-      Returns the list of known items.
-      Note that the list is updated only when the loggers had been called already.
+      Returns
+      -------
+      vector<int>*
+          A pointer to the list of known items. In the online experiment, the list is updated by this class for each sample after the call of the loggers and before the call to the updaters.
     */
     vector<int>* get_users() { return &users_; } //TODO return const value, classes should not change the vector
     /**
-      Returns the list of known users.
-      Note that the list is updated only when the loggers had been called already.
+      Returns
+      -------
+      vector<int>*
+          A pointer to the list of known users.
     */
     SpMatrix* get_train_matrix() { return &train_matrix_; } //TODO const
     /**
-      Returns the current train data in a sparse matrix form.
-      Note that the matrix is updated only when the loggers had been called already.
+      Returns
+      -------
+      SpMatrix*
+          A pointer to the current training data in a sparse matrix form.
     */
     PopContainer* get_popularity_container() { return &popularity_container_; } //TODO const
     /**
-      Returns a container containing the popularity statistics of known items.
-      Note that the container is updated only when the loggers had been called already.
+      Returns
+      -------
+      PopContainer*
+          A pointer to a container containing the popularity statistics of known items.
     */
     TopPopContainer* get_popularity_sorted_container() { return &popularity_sorted_container_; } //TODO const
     /**
-      Returns a container containing the popularity statistics of known items.
-      The items can be acquired in popularity order.
-      Note that the container is updated only when the loggers had been called already.
+      Returns
+      -------
+      TopPopContainer*
+          A pointer to a container containing the popularity statistics of known items.  The items can be acquired in popularity order.
     */
 
     //update
@@ -95,6 +133,13 @@ class ExperimentEnvironment{
     /**
       update(RecDat* rec_dat)
       Updates the container.
+      
+      In the online experiment, :py:class:`alpenglow.cpp.OnlineExperiment` calls this function after the call to the loggers and before the call to the updaters. Other classes should not call this function.
+      Parameters
+      ----------
+      rec_dat : RecDat*
+         The newest available sample of the experiment.
+
     */
 
   private:
