782e2926134a833414f6f1c4bfec33e031f3c4e4
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-21 18:00:57 +0100

    OnlineExperiment: rewrite documentation following better style

--- cpp/src/main/online_experiment/OnlineExperiment.h
+++ cpp/src/main/online_experiment/OnlineExperiment.h
@@ -25,31 +25,61 @@ struct OnlineExperimentParameters{
 
 class OnlineExperiment{
   /**
-  The central class of the experiment.
-  Calls the loggers and updaters for each sample, and updates experiment_environment.
+  The central class of the online experiment.
+  
+  It queries samples from the dataset, then one-by-one for each sample
 
-  //See anatomy_of_experiment for details. //TODO
-  See :py:class:`alpenglow.OnlineExperiment.OnlineExperiment`.
+  * calls loggers that are set using :py:meth:`add_logger`,
+  * updates the environment and common statistics, see :py:class:`alpenglow.cpp.ExperimentEnvironment`,
+  * calls the updaters that are set using :py:math:`add_updater`.
+
+  At the end of the experiment, it calls end loggers that are set using :py:meth:`add_end_logger`.
+
+  //See anatomy_of_experiment for details. //TODO how to link?
+  See :py:class:`alpenglow.OnlineExperiment.OnlineExperiment` for details.
   */
   public:
     OnlineExperiment(OnlineExperimentParameters* parameters);
     ~OnlineExperiment();
     void add_logger(Logger* logger){loggers_.push_back(logger);}
     /**
-    Add a logger instance.
-    Loggers will be called for each sample before updating the environment and calling updaters.
+      add_logger(Logger* logger)
+      Adds a logger instance.
+
+      Parameters
+      ----------
+      logger : Logger*
+          Pointer to the logger to be added.
     */
     void add_end_logger(Logger* logger){end_loggers_.push_back(logger);}
     /**
-    Add a logger instance, that will be called once at the end of the experiment.
+      add_end_logger(Logger* logger)
+      Adds a logger instance, that will be called once at the end of the experiment.
+
+      Parameters
+      ----------
+      logger : Logger*
+          Pointer to the logger to be added.
     */
     void add_updater(Updater* updater){ updaters_.push_back(updater); }
     /**
-    Add an updater. Updaters will be called for each sample after updting the environment.
+      add_updater(Updater* updater)
+      Adds an updater.
+
+      Parameters
+      ----------
+      updater : Updater*
+          Pointer to the updater to be added.
     */
     void set_recommender_data_iterator(RecommenderDataIterator* recommender_data_iterator);
     /**
-    Set the dataset of the experiment.
+      set_recommender_data_iterator(RecommenderDataIterator* recommender_data_iterator)
+      Sets the dataset of the experiment.
+
+      Parameters
+      ----------
+      recommender_data_iterator : RecommenderDataIterator*
+          Pointer to the dataset.
     */
     bool self_test(){
       bool ok = true;
@@ -63,11 +93,31 @@ class OnlineExperiment{
       }
       return ok;
     }
+    /**
+      Tests if the dataset is set.
+
+      Furthermore, the test produces a warning message if no loggers are set
+      because in that case the the experiment will produce no output. 
+
+      Returns
+      -------
+      bool
+         Whether the tests were successful. 
+    */
     void run(); //SIP_THROW (PythonKeyboardInterruptException) /ReleaseGIL/
     /**
-    Run the experiment.
+      Runs the experiment.
     */
     void inject_experiment_environment_into(NeedsExperimentEnvironment *object);
+    /**
+      inject_experiment_environment_into(NeedsExperimentEnvironment *object)
+      Sets the experiment environment into another object that requires it.
+
+      In the online experiment, this method is automatically called with all the
+      objects that implement :py:class:`alpenglow.cpp.NeedsExperimentEnvironment`,
+      injecting the dependency where it is necessary. See the code of
+      :py:class:`alpenglow.OnlineExperiment.OnlineExperiment` for details.
+    */
   private:
     ExperimentEnvironment* experiment_environment_;
     RecommenderDataIterator* recommender_data_iterator_ = NULL;
