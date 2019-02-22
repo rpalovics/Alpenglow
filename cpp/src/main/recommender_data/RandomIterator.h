83b323bfbfbe1aff493d6997ec0f3ffa3a0b79cf
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-22 13:12:43 +0100

    RandomIterator: fix formatting and readability

--- cpp/src/main/recommender_data/RandomIterator.h
+++ cpp/src/main/recommender_data/RandomIterator.h
@@ -9,39 +9,40 @@
 #include "RecommenderDataIterator.h"
 #include "../utils/Random.h"
 
-struct RandomIteratorParameters{
+struct RandomIteratorParameters {
   int seed;
   string shuffle_mode = "auto_shuffle";
 };
 class RandomIterator : public RecommenderDataIterator {
-  public:
-    RandomIterator(RecommenderData* recommender_data, int seed, string shuffle_mode){
-      recommender_data_ = recommender_data;
-      random_.set(seed);
-      shuffle_mode_ = shuffle_mode;
-      if (!initialize()){
-        cerr << "RandomIterator: initialization was no successful." << endl;
-        throw 0;
-      }
+public:
+  RandomIterator(RecommenderData* recommender_data, int seed, string shuffle_mode){
+    recommender_data_ = recommender_data;
+    random_.set(seed);
+    shuffle_mode_ = shuffle_mode;
+    bool init_success = initialize();
+    if (!init_success){
+      cerr << "RandomIterator: initialization was not successful." << endl;
+      throw 0;
     }
-    RandomIterator(RandomIteratorParameters* params){
-      random_.set(params->seed);
-      shuffle_mode_=params->shuffle_mode;
-    }
-    RecDat* next();
-    void restart();
-    void shuffle();
-    RecDat* get_actual();
-    RecDat* get(int index) const;
-    RecDat* get_future(int index) const;
-    double get_following_timestamp() const;
-  private:
-    Random random_;
-    vector<RecDat*> shuffled_data_;
-    string shuffle_mode_ = "auto_shuffle"; //auto_shuffle or manual_shuffle
+  }
+  RandomIterator(RandomIteratorParameters* params){
+    random_.set(params->seed);
+    shuffle_mode_=params->shuffle_mode;
+  }
+  RecDat* next();
+  void restart();
+  void shuffle();
+  RecDat* get_actual();
+  RecDat* get(int index) const;
+  RecDat* get_future(int index) const;
+  double get_following_timestamp() const;
+private:
+  Random random_;
+  vector<RecDat*> shuffled_data_;
+  string shuffle_mode_ = "auto_shuffle"; //auto_shuffle or manual_shuffle
 
-    bool autocalled_initialize() override;
-    bool parent_is_initialized_ = false;
+  bool autocalled_initialize() override;
+  bool parent_is_initialized_ = false;
 };
 
 
