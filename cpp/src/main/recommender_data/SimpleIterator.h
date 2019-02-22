92ce2451a5e7b32d9d837544b9e7e2e775cc378c
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-22 13:06:56 +0100

    SimpleIterator: replace repeadted doc with link to parent

--- cpp/src/main/recommender_data/SimpleIterator.h
+++ cpp/src/main/recommender_data/SimpleIterator.h
@@ -17,62 +17,25 @@ public:
   }
   RecDat* next() override;
   /**
-    Advances the iterator and returns a pointer to the following sample.
-  
-    Returns
-    -------
-    RecDat*
-        A pointer to the following sample.
+    See :py:meth:`alpenglow.cpp.RecommenderDataIterator.next()`
   */
   RecDat* get(int index) const override;
   /**
     get(int index)
-    This method provides random access to the past samples of the time series.
-    It reaches an error if index is larger than the index of the current sample,
-    i.e. if one tries to access data from the future through this function.
-
-    Use :py:meth:`get_counter` to get the index of the newest available sample.
-    Use :py:meth:`get_future` to get data from the future.
-
-    Parameters
-    ----------
-    index : int
-        The index of sample to return.
-    Returns
-    ------
-    RecDat*
-        A pointer to the sample.
+    See :py:meth:`alpenglow.cpp.RecommenderDataIterator.get()`
   */
   RecDat* get_actual() override;
   /**
-    Returns
-    ------
-    RecDat*
-        A pointer to the actual sample.
+    See :py:meth:`alpenglow.cpp.RecommenderDataIterator.get_actual()`
   */
   RecDat* get_future(int index) const override;
   /**
     get_future(int index)
-    This method provides random access to the complete time series, including
-    future.
-
-    Use :py:meth:`get` to safely get data from the past.
-
-    Parameters
-    ----------
-    index : int
-        The index of sample to return.
-    Returns
-    ------
-    RecDat*
-        A pointer to the sample.
+    See :py:meth:`alpenglow.cpp.RecommenderDataIterator.get_future()`
   */
   double get_following_timestamp() const override;
   /**
-    Returns
-    -------
-    double
-        The timestamp of the next sample in the future, i.e., when will the next event happen.
+    See :py:meth:`alpenglow.cpp.RecommenderDataIterator.get_following_timestamp()`
   */
 };
 
