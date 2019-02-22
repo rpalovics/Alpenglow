737d38686b2dc07642fa5923cbdf0ebf557c9d7b
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-02-22 11:59:48 +0100

    RecommenderDataIterator: add doc comments

--- cpp/src/main/recommender_data/RecommenderDataIterator.h
+++ cpp/src/main/recommender_data/RecommenderDataIterator.h
@@ -6,22 +6,113 @@
 #ifndef RECOMMENDERDATAITERATOR_H_
 #define RECOMMENDERDATAITERATOR_H_
 
+//SIP_AUTOCONVERT
+
 #include "RecommenderData.h"
 #include "../utils/SpMatrix.h"
 
-class RecommenderDataIterator : public Initializable{
+class RecommenderDataIterator : public Initializable {
+/**
+  Iterator-like interface that serves the dataset as a time series in the online
+  experiment. The class also provides random access to the time series.
+  Implementations assume that the data is already sorted by time.
+*/
 public:
   void set_recommender_data(RecommenderData* data){ recommender_data_=data; }
+  /**
+    set_recommender_data(RecommenderData* data)
+    Sets the dataset that we iterate on.
+
+    Parameters
+    ----------
+    data : RecommenderData*
+        The dataset.
+  */
   virtual bool has_next() const { return counter_ < recommender_data_->size(); } //iterator
+  /**
+    Returns
+    -------
+    bool
+        Whether the iterator has't reached the end of the time series.
+  */
   virtual RecDat* next() = 0; //iterator
+  /**
+    Advances the iterator and returns a pointer to the following sample.
+
+    Returns
+    -------
+    RecDat*
+        A pointer to the following sample.
+  */
   virtual int get_counter() const { return counter_-1; } //get the index of actual sample
+  /**
+    Returns
+    ------
+    int
+        Index of the actual sample.
+  */
   virtual RecDat* get_actual() = 0; //get actual sample
+  /**
+    Returns
+    ------
+    RecDat*
+        A pointer to the actual sample.
+  */
   virtual RecDat* get(int index) const = 0; //index'th sample from recommender_data
+  /**
+    get(int index)
+    This method provides random access to the past samples of the time series.
+    It reaches an error if index is larger than the index of the current sample,
+    i.e. if one tries to access data from the future through this function.
+
+    Use :py:meth:`get_counter` to get the index of the newest available sample.
+    Use :py:meth:`get_future` to get data from the future.
+
+    Parameters
+    ----------
+    index : int
+        The index of sample to return.
+    Returns
+    ------
+    RecDat*
+        A pointer to the sample.
+  */
   virtual RecDat* get_future(int index) const = 0; //index'th sample from recommender_data, getting sample from future is allowed
+  /**
+    get_future(int index)
+    This method provides random access to the complete time series, including
+    future.
+
+    Use :py:meth:`get` to safely get data from the past.
+
+    Parameters
+    ----------
+    index : int
+        The index of sample to return.
+    Returns
+    ------
+    RecDat*
+        A pointer to the sample.
+  */
   virtual int size() const { return recommender_data_->size(); } //number of samples
+  /**
+    Returns
+    -------
+    int
+        The number of the samples.
+  */
   virtual double get_following_timestamp() const = 0; //returns timestamp of the following sample, returns -1 in case of last sample
-  virtual void restart() { counter_ = 0; }
-  virtual ~RecommenderDataIterator() {}
+  /**
+    Returns
+    -------
+    double
+        The timestamp of the next sample in the future, i.e., when will the next event happen.
+  */
+  virtual void restart(){ counter_ = 0; }
+  /**
+    Restarts the iterator.
+  */
+  virtual ~RecommenderDataIterator(){}
 protected:
   bool autocalled_initialize() override {
     return recommender_data_->is_initialized();
@@ -31,4 +122,4 @@ protected:
 };
 
 
-#endif /* IRECOMMENDERDATAITERATOR_H_ */
+#endif /* RECOMMENDERDATAITERATOR_H_ */
