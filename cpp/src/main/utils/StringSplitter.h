10a5c7b185fe50d585faedf6035cc42f731ad9cd
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-03-08 12:23:46 +0100

    remove unused TrackDataContainer and StringSplitter

--- cpp/src/main/utils/StringSplitter.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRING_SPLITTER
-#define STRING_SPLITTER
-#include <vector>
-#include <string>
-
-std::vector<std::string> split(const std::string& input, const char sep);
-
-#endif /* STRING_SPLITTER */
