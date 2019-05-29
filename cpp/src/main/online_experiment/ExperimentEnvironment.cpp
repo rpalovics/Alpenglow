aea00d7d8d574d2b1a2fa986b6c36375eedf39c8
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-05-29 16:19:46 +0200

    ExpEnv: fill at the beginning users_ and items_ if initall

--- cpp/src/main/online_experiment/ExperimentEnvironment.cpp
+++ cpp/src/main/online_experiment/ExperimentEnvironment.cpp
@@ -10,6 +10,12 @@ void ExperimentEnvironment::set_parameters(OnlineExperimentParameters* params){
   initialize_all_ = params->initialize_all;
   max_user_ = params->max_user;
   max_item_ = params->max_item;
+  if (initialize_all_ && max_user_>0){
+    for(int i=0;i<=max_user_;i++) users_.push_back(i);
+  }
+  if (initialize_all_ && max_item_>0){
+    for(int i=0;i<=max_item_;i++) items_.push_back(i);
+  }
 }
 void ExperimentEnvironment::update(RecDat* rec_dat){
   int item = rec_dat->item;
@@ -19,12 +25,12 @@ void ExperimentEnvironment::update(RecDat* rec_dat){
   if (!GET_VECTORMAP(item_map_,item,false)){
     new_item_=true;
     PUT_VECTORMAP(item_map_,item,true);
-    items_.push_back(item);
+    if (!initialize_all_) items_.push_back(item);
   } else {
     new_item_=false;
   }
   if (!GET_VECTORMAP(user_map_,user,false)){
-    new_user_=true;
+    first_occurrence_of_user_=true;
     PUT_VECTORMAP(user_map_,user,true);
     users_.push_back(user);
   } else {
