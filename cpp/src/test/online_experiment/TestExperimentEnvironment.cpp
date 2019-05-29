6c07539138ad1f80562b5f112aafcaf46a59ef57
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-05-29 18:56:55 +0200

    ExpEnv: add tests for queries and gettable objects

--- cpp/src/test/online_experiment/TestExperimentEnvironment.cpp
+++ cpp/src/test/online_experiment/TestExperimentEnvironment.cpp
@@ -27,7 +27,6 @@ public:
       rec_dats[i].id = i;
       rec_dats[i].eval = 1;
     }
-    for(auto rec_dat : rec_dats){ cerr << rec_dat << endl; }
     recommender_data.set_rec_data(rec_dats);
     rdi.set_recommender_data(&recommender_data);
     params.top_k = 123;
@@ -157,40 +156,56 @@ TEST_F(TestExperimentEnvironment, new_user_item) {
   expenv.set_recommender_data_iterator(&rdi);
   EXPECT_TRUE(expenv.self_test());
 
-  expenv.update(rdi.next());
+  for (int i=0;i<10;i++) EXPECT_FALSE(expenv.is_item_existing(i));
+  for (int i=0;i<10;i++) EXPECT_FALSE(expenv.is_user_existing(i));
+  expenv.update(rdi.next()); //id=0 t=0 u=0 i=0 s=1
   EXPECT_TRUE(expenv.is_new_item());
   EXPECT_TRUE(expenv.is_first_occurrence_of_item());
   EXPECT_TRUE(expenv.is_new_user());
   EXPECT_TRUE(expenv.is_first_occurrence_of_user());
   EXPECT_TRUE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=1 t=1 u=0 i=0 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_FALSE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_FALSE(expenv.is_first_occurrence_of_user());
   EXPECT_FALSE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=2 t=2 u=0 i=0 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_FALSE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_FALSE(expenv.is_first_occurrence_of_user());
   EXPECT_FALSE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=3 t=3 u=0 i=1 s=1
   EXPECT_TRUE(expenv.is_new_item());
   EXPECT_TRUE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_FALSE(expenv.is_first_occurrence_of_user());
   EXPECT_TRUE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=4 t=4 u=1 i=1 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_FALSE(expenv.is_first_occurrence_of_item());
   EXPECT_TRUE(expenv.is_new_user());
   EXPECT_TRUE(expenv.is_first_occurrence_of_user());
   EXPECT_TRUE(expenv.is_item_new_for_user());
+  for (int i=0;i<2;i++) EXPECT_TRUE(expenv.is_item_existing(i));
+  for (int i=2;i<10;i++) EXPECT_FALSE(expenv.is_item_existing(i));
+  for (int i=0;i<2;i++) EXPECT_TRUE(expenv.is_user_existing(i));
+  for (int i=2;i<10;i++) EXPECT_FALSE(expenv.is_user_existing(i));
+
+  expenv.update(rdi.next()); //id=5 t=5 u=1 i=1 s=1
+  expenv.update(rdi.next()); //id=6 t=6 u=1 i=2 s=1
+  expenv.update(rdi.next()); //id=7 t=7 u=1 i=2 s=1
+  expenv.update(rdi.next()); //id=8 t=8 u=2 i=2 s=1
+  expenv.update(rdi.next()); //id=9 t=9 u=2 i=3 s=1
+  for (int i=0;i<4;i++) EXPECT_TRUE(expenv.is_item_existing(i));
+  for (int i=4;i<10;i++) EXPECT_FALSE(expenv.is_item_existing(i));
+  for (int i=0;i<3;i++) EXPECT_TRUE(expenv.is_user_existing(i));
+  for (int i=3;i<10;i++) EXPECT_FALSE(expenv.is_user_existing(i));
 }
 
 TEST_F(TestExperimentEnvironment, new_user_item_initall) {
@@ -214,44 +229,216 @@ TEST_F(TestExperimentEnvironment, new_user_item_initall) {
   expenv.set_recommender_data_iterator(&rdi);
   EXPECT_TRUE(expenv.self_test());
 
-  expenv.update(rdi.next());
+  for (int i=0;i<4;i++) EXPECT_TRUE(expenv.is_item_existing(i));
+  for (int i=4;i<10;i++) EXPECT_FALSE(expenv.is_item_existing(i));
+  for (int i=0;i<3;i++) EXPECT_TRUE(expenv.is_user_existing(i));
+  for (int i=3;i<10;i++) EXPECT_FALSE(expenv.is_user_existing(i));
+
+  expenv.update(rdi.next()); //id=0 t=0 u=0 i=0 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_TRUE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_TRUE(expenv.is_first_occurrence_of_user());
   EXPECT_TRUE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=1 t=1 u=0 i=0 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_FALSE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_FALSE(expenv.is_first_occurrence_of_user());
   EXPECT_FALSE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=2 t=2 u=0 i=0 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_FALSE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_FALSE(expenv.is_first_occurrence_of_user());
   EXPECT_FALSE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=3 t=3 u=0 i=1 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_TRUE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_FALSE(expenv.is_first_occurrence_of_user());
   EXPECT_TRUE(expenv.is_item_new_for_user());
 
-  expenv.update(rdi.next());
+  expenv.update(rdi.next()); //id=4 t=4 u=1 i=1 s=1
   EXPECT_FALSE(expenv.is_new_item());
   EXPECT_FALSE(expenv.is_first_occurrence_of_item());
   EXPECT_FALSE(expenv.is_new_user());
   EXPECT_TRUE(expenv.is_first_occurrence_of_user());
   EXPECT_TRUE(expenv.is_item_new_for_user());
+
+  for (int i=0;i<4;i++) EXPECT_TRUE(expenv.is_item_existing(i));
+  for (int i=4;i<10;i++) EXPECT_FALSE(expenv.is_item_existing(i));
+  for (int i=0;i<3;i++) EXPECT_TRUE(expenv.is_user_existing(i));
+  for (int i=3;i<10;i++) EXPECT_FALSE(expenv.is_user_existing(i));
 }
+TEST_F(TestExperimentEnvironment, users_items_noinitall) {
+  //data:
+  //id=0 t=0 u=0 i=0 s=1
+  //id=1 t=1 u=0 i=0 s=1
+  //id=2 t=2 u=0 i=0 s=1
+  //id=3 t=3 u=0 i=1 s=1
+  //id=4 t=4 u=1 i=1 s=1
+  //id=5 t=5 u=1 i=1 s=1
+  //id=6 t=6 u=1 i=2 s=1
+  //id=7 t=7 u=1 i=2 s=1
+  //id=8 t=8 u=2 i=2 s=1
+  //id=9 t=9 u=2 i=3 s=1
 
-//TODO test items, users, train_matrix, rdi
+  params.initialize_all = false;
+  params.max_user = 2;
+  params.max_item = 3;
+  ExperimentEnvironment expenv;
+  expenv.set_parameters(&params);
+  expenv.set_recommender_data_iterator(&rdi);
+  EXPECT_TRUE(expenv.self_test());
+  const vector<int>* items = expenv.get_items();
+  const vector<int>* users = expenv.get_users();
 
+  EXPECT_EQ(0,items->size());
+  EXPECT_EQ(0,users->size());
+  
+  expenv.update(rdi.next()); //id=0 t=0 u=0 i=0 s=1
+  EXPECT_EQ(1,items->size());
+  EXPECT_EQ(1,users->size());
+
+  expenv.update(rdi.next()); //id=1 t=1 u=0 i=0 s=1
+  expenv.update(rdi.next()); //id=2 t=2 u=0 i=0 s=1
+  expenv.update(rdi.next()); //id=3 t=3 u=0 i=1 s=1
+  EXPECT_EQ(2,items->size());
+  EXPECT_EQ(1,users->size());
+
+  expenv.update(rdi.next()); //id=4 t=4 u=1 i=1 s=1
+  EXPECT_EQ(2,items->size());
+  EXPECT_EQ(2,users->size());
+
+  expenv.update(rdi.next()); //id=5 t=5 u=1 i=1 s=1
+  expenv.update(rdi.next()); //id=6 t=6 u=1 i=2 s=1
+  EXPECT_EQ(3,items->size());
+  EXPECT_EQ(2,users->size());
+
+  expenv.update(rdi.next()); //id=7 t=7 u=1 i=2 s=1
+  expenv.update(rdi.next()); //id=8 t=8 u=2 i=2 s=1
+  EXPECT_EQ(3,items->size());
+  EXPECT_EQ(3,users->size());
+
+  expenv.update(rdi.next()); //id=9 t=9 u=2 i=3 s=1
+  EXPECT_EQ(4,items->size());
+  EXPECT_EQ(3,users->size());
+  EXPECT_EQ(0,items->at(0));
+  EXPECT_EQ(1,items->at(1));
+  EXPECT_EQ(2,items->at(2));
+  EXPECT_EQ(3,items->at(3));
+  EXPECT_EQ(0,users->at(0));
+  EXPECT_EQ(1,users->at(1));
+  EXPECT_EQ(2,users->at(2));
+}
+TEST_F(TestExperimentEnvironment, users_items_initall) {
+  //data:
+  //id=0 t=0 u=0 i=0 s=1
+  //id=1 t=1 u=0 i=0 s=1
+  //id=2 t=2 u=0 i=0 s=1
+  //id=3 t=3 u=0 i=1 s=1
+  //id=4 t=4 u=1 i=1 s=1
+  //id=5 t=5 u=1 i=1 s=1
+  //id=6 t=6 u=1 i=2 s=1
+  //id=7 t=7 u=1 i=2 s=1
+  //id=8 t=8 u=2 i=2 s=1
+  //id=9 t=9 u=2 i=3 s=1
+
+  params.initialize_all = true;
+  params.max_user = 2;
+  params.max_item = 3;
+  ExperimentEnvironment expenv;
+  expenv.set_parameters(&params);
+  expenv.set_recommender_data_iterator(&rdi);
+  EXPECT_TRUE(expenv.self_test());
+  const vector<int>* items = expenv.get_items();
+  const vector<int>* users = expenv.get_users();
+  while(rdi.has_next()){
+    EXPECT_EQ(4,items->size());
+    EXPECT_EQ(0,items->at(0));
+    EXPECT_EQ(1,items->at(1));
+    EXPECT_EQ(2,items->at(2));
+    EXPECT_EQ(3,items->at(3));
+    EXPECT_EQ(3,users->size());
+    EXPECT_EQ(0,users->at(0));
+    EXPECT_EQ(1,users->at(1));
+    EXPECT_EQ(2,users->at(2));
+    expenv.update(rdi.next());
+  }
+}
+TEST_F(TestExperimentEnvironment, train_matrix) {
+  //data:
+  //id=0 t=0 u=0 i=0 s=1
+  //id=1 t=1 u=0 i=0 s=1
+  //id=2 t=2 u=0 i=0 s=1
+  //id=3 t=3 u=0 i=1 s=1
+  //id=4 t=4 u=1 i=1 s=1
+  //id=5 t=5 u=1 i=1 s=1
+  //id=6 t=6 u=1 i=2 s=1
+  //id=7 t=7 u=1 i=2 s=1
+  //id=8 t=8 u=2 i=2 s=1
+  //id=9 t=9 u=2 i=3 s=1
+
+  params.initialize_all = false;
+  params.max_user = 2;
+  params.max_item = 3;
+  ExperimentEnvironment expenv;
+  expenv.set_parameters(&params);
+  expenv.set_recommender_data_iterator(&rdi);
+  EXPECT_TRUE(expenv.self_test());
+
+  const SpMatrix* train_matrix = expenv.get_train_matrix();
+
+  EXPECT_EQ(0,train_matrix->size());
+
+  expenv.update(rdi.next()); //id=0 t=0 u=0 i=0 s=1
+  EXPECT_EQ(1, train_matrix->size());
+  EXPECT_EQ(1, train_matrix->row_size(0));
+  EXPECT_EQ(1, train_matrix->get(0,0));
+
+  expenv.update(rdi.next()); //id=1 t=1 u=0 i=0 s=1
+  expenv.update(rdi.next()); //id=2 t=2 u=0 i=0 s=1
+  expenv.update(rdi.next()); //id=3 t=3 u=0 i=1 s=1
+  EXPECT_EQ(1, train_matrix->size());
+  EXPECT_EQ(2, train_matrix->row_size(0));
+  EXPECT_EQ(1, train_matrix->get(0,0));
+  EXPECT_EQ(1, train_matrix->get(0,1));
+
+  expenv.update(rdi.next()); //id=4 t=4 u=1 i=1 s=1
+  EXPECT_EQ(2, train_matrix->size());
+  EXPECT_EQ(2, train_matrix->row_size(0));
+  EXPECT_EQ(1, train_matrix->row_size(1));
+  EXPECT_EQ(1, train_matrix->get(0,0));
+  EXPECT_EQ(1, train_matrix->get(0,1));
+  EXPECT_EQ(1, train_matrix->get(1,1));
+}
+TEST_F(TestExperimentEnvironment, rdi) {
+  //data:
+  //id=0 t=0 u=0 i=0 s=1
+  //id=1 t=1 u=0 i=0 s=1
+  //id=2 t=2 u=0 i=0 s=1
+  //id=3 t=3 u=0 i=1 s=1
+  //id=4 t=4 u=1 i=1 s=1
+  //id=5 t=5 u=1 i=1 s=1
+  //id=6 t=6 u=1 i=2 s=1
+  //id=7 t=7 u=1 i=2 s=1
+  //id=8 t=8 u=2 i=2 s=1
+  //id=9 t=9 u=2 i=3 s=1
+
+  params.initialize_all = false;
+  params.max_user = 2;
+  params.max_item = 3;
+  ExperimentEnvironment expenv;
+  expenv.set_parameters(&params);
+  expenv.set_recommender_data_iterator(&rdi);
+  EXPECT_TRUE(expenv.self_test());
+  const RecommenderDataIterator* rdi2 = expenv.get_recommender_data_iterator();
+  EXPECT_EQ(&rdi,rdi2);
+}
 int main (int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
