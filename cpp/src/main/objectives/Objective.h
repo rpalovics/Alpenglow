213921abd2b43f3611cb3ee9a7bc60653cff4659
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-06-07 13:13:11 +0200

    create self_test() in anchestor classes

--- cpp/src/main/objectives/Objective.h
+++ cpp/src/main/objectives/Objective.h
@@ -10,6 +10,7 @@ class ObjectivePointWise{
   public:
     virtual ~ObjectivePointWise(){}
     virtual double get_gradient(RecPred* rec_pred)=0;
+    bool self_test(){ return true; }
 };
 
 
@@ -17,11 +18,13 @@ class ObjectivePairWise{ //SIP_ABSTRACT
   public:
     virtual ~ObjectivePairWise(){}
     virtual pair<double,double> get_gradient(RecPred* rec_pred1, RecPred* rec_pred2)=0;
+    bool self_test(){ return true; }
 };
 
 class ObjectiveListWise{
   public:
     virtual ~ObjectiveListWise(){}
     virtual vector<double>  get_gradient(vector<RecPred>* predictions)=0;
+    bool self_test(){ return true; }
 };
 #endif /* OBJECTIVE_POINT_WISE_H */
