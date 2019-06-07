463053daefcf23505290a75eb538c025fe256c58
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-06-07 12:03:24 +0200

    add boundary check to avoid segmentation fault danger

--- cpp/src/main/loggers/ListConditionalMetaLogger.cpp
+++ cpp/src/main/loggers/ListConditionalMetaLogger.cpp
@@ -1,7 +1,7 @@
 #include "ListConditionalMetaLogger.h"
 
 bool ListConditionalMetaLogger::should_run(RecDat* recDat){
-  bool should = should_run_vector_[position];
+  bool should = should_run_vector_.at(position);
   position+=1;
   return should;
 }
