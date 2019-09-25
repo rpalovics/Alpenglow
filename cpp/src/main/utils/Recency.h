b0f4e6f26b66d0e2d0539cc1695001e16050fd6c
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-09-25 11:09:36 +0200

    standardize coding style

--- cpp/src/main/utils/Recency.h
+++ cpp/src/main/utils/Recency.h
@@ -10,27 +10,25 @@
 
 class Recency{
   public:
-    Recency(){};
-    ~Recency(){};
+    virtual ~Recency(){}
     virtual double get(int entity, double actual_time) = 0;
     virtual void update(int entity, double actual_time) = 0;
   private:
 };
 
 struct PowerLawRecencyParameters {
-  double delta_t;
-  double exponent;
+  double delta_t = -1;
+  double exponent = -1;
 };
 
 class PowerLawRecency : public Recency{
   public:
-    PowerLawRecency(PowerLawRecencyParameters * params){
+    PowerLawRecency(PowerLawRecencyParameters* params){
       delta_t_ = params->delta_t;
       exponent_ = params->exponent;
     }
-    ~PowerLawRecency(){};
-    double get(int entity, double actual_time);
-    void update(int entity, double actual_time);
+    double get(int entity, double actual_time) override;
+    void update(int entity, double actual_time) override;
   private:
     std::map <int,double> prev_time_;
     double delta_t_;
