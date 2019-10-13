796d80eb3fdf3d1ce2095c03a91bd0f3db5caee7
Erzsebet Frigo <fbobee@info.ilab.sztaki.hu>
2019-10-13 18:31:50 +0200

    create better interface instead of ModelFilter

---
+++ cpp/src/main/filters/WhiteListFilter.h
@@ -0,0 +1,39 @@
+#ifndef WHITE_LIST_FILTER
+#define WHITE_LIST_FILTER
+
+#include <vector>
+#include "../recommender_data/RecommenderData.h"
+
+//SIP_AUTOCONVERT
+
+class WhiteListFilter { //SIP_ABSTRACT
+/**
+  Filter interface for classes that implement white list type filtering.
+*/
+public:
+  virtual bool active(RecDat* rec_dat) = 0;
+  /**
+    Parameters
+    ----------
+    rec_dat : RecDat*
+       The sample containing the user and the item.
+    Returns
+    -------
+    bool
+       Whether the item is available for the user.
+  */
+  virtual vector<int> get_white_list(int user) = 0;
+  /**
+    Parameters
+    ----------
+    user : int
+       The whitelist will be computed for the given user.
+    Returns
+    -------
+    vector<int,double>
+       The list of allowed items for the given user. The second element of the pair is an upper bound for the score of the item for the given user (or the score itself).
+  */
+  virtual ~WhiteListFilter(){}
+};
+
+#endif //WHITE_LIST_FILTER_H
